c0700537fe0414f27bf0e06f8ee0d0f5a23b9100
Guillermo Jarana <guillermo.jarana-barragan@iesruizgijon.com>
2022-05-23 19:49:53 +0200

    Calculadora sin métodos

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,50 @@
+
+package calculadora;
+
+/**
+ *
+ * @author Guillermo Jarana
+ */
+public class Calculadora {
+    // clase con constructor pasando dos numeros reales, setters , getters y métodos para devolver el resultado de sumar, restar , multiplicar y dividir
+    private String nombre;
+    private double numero1, numero2;
+
+    public Calculadora(double numero1, double numero2){
+        this.numero1=numero1;
+        this.numero2=numero2;
+    }
+
+    public Calculadora(String nombre, double numero1, double numero2) {
+        this.nombre = nombre;
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public Calculadora() {
+    }
+
+    // sets and gets
+
+    public void setNumero1(double numero1) {
+        this.numero1=numero1;
+    }
+    public void setNumero2(double numero2) {
+        this.numero2=numero2;
+    }
+
+    public double getNumero1(){
+        return numero1;
+    }
+    public double getNumero2(){
+        return numero2;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
